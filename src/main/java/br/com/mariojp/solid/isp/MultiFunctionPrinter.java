b41266249f31e446348802c795c285c70ab3973b
Bernardo Megale Amorim <bernardomegale.amorim@ucsal.edu.brhistory>
2025-09-02 14:51:38 -0300

    Refatoração ISP - OfficeService agora depende apenas de Printer

---
+++ src/main/java/br/com/mariojp/solid/isp/MultiFunctionPrinter.java
@@ -0,0 +1,12 @@
+package br.com.mariojp.solid.isp;
+
+public class MultiFunctionPrinter implements Printer, Scanner {
+
+    @Override
+    public void print(String content) {
+    }
+
+    @Override
+    public void scan(String targetFile) {
+    }
+}
